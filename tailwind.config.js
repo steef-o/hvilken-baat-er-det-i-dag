fb1de34d878969060b640f2b0dccd8620341ee80
Steffen Hagen <steffen.hagen@webstep.no>
2022-06-25 13:45:03 +0200

    Add fonts and colors to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./src/**/*.{ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ['Poppins', "sans-serif"],
+        "mono": ["PT Mono", "monospace"]
+      }
+    },
+    colors: {
+      "midnight-blue": "#151C35",
+      "lollipop": "#EE709D",
+      "grape": "#65506E",
+      "independence": "#464C70",
+      "ocean-blue": "#254CC3",
+      "vista-blue": "#8297E2",
+    }
   },
   plugins: [],
 }
