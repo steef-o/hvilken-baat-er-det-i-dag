01902f8eff9d37a60306c50b8d3da8ce87e61daa
Steffen Hagen <steffen.hagen@webstep.no>
2022-06-25 13:17:25 +0200

    Add eslint ruiles and additional settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
         <p className="text-red-300">Hello Vite + React!</p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
