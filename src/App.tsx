f66216094eb6f28feb01ab31b6fcbac5276fc115
Steffen Hagen <steffen.hagen@webstep.no>
2022-06-25 14:04:52 +0200

    Create styles for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-red-300">Hello Vite + React!</p>
+    <div className="h-screen bg-base">
+      <header className="h-16 items-center bg-midnight-blue text-center">
+        <h1 className="pt-4 align-middle text-xl uppercase text-dirt">Hvilken Båt er det i dag</h1>
       </header>
+      <main className="mx-auto max-w-project"></main>
     </div>
   );
 }
