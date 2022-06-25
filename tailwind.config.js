6461a107a8e65a005788ee6c438da587f8f32fa9
Steffen Hagen <steffen.hagen@webstep.no>
2022-06-25 14:04:37 +0200

    Add tailwind configs and prettier plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,18 @@ module.exports = {
       }
     },
     colors: {
+      "dirt": "#D9D9D9",
+      "base": "#252B4F",
       "midnight-blue": "#151C35",
       "lollipop": "#EE709D",
       "grape": "#65506E",
       "independence": "#464C70",
       "ocean-blue": "#254CC3",
       "vista-blue": "#8297E2",
-    }
+    },
+    maxWidth: {
+      "project": "800px"
+    },
   },
   plugins: [],
 }
