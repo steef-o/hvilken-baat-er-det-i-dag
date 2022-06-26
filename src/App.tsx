905a332c350ef69123c672dfb8baaa4d2267904e
Steffen Hagen <steffen.hagen@webstep.no>
2022-06-26 14:58:23 +0200

    Style adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import DailyMessage from "~/components/DailyMessage";
 
 const App = () => (
   <Suspense fallback={<div className="bg-base">Loading...</div>}>
-    <div className="bg-base pb-20">
+    <div className="min-h-screen bg-base pb-20">
       <header className="h-16 items-center bg-midnight-blue text-center">
         <h1 className="pt-4 align-middle text-xl uppercase text-dirt">Hvilken Båt er det i dag</h1>
       </header>
-      <main className="mx-auto mb-20 max-w-project">
+      <main className="mx-auto mb-20 max-w-project px-4">
         <Toolbar />
         <DailyMessage />
         {/* @TODO create animation for ship*/}
--- src/components/shipList/ShipListItem.tsx
+++ src/components/shipList/ShipListItem.tsx
@@ -11,43 +11,45 @@ interface ShipListItemProps {
 }
 
 const ShipListItem = ({ ship, anchoredShip = false }: ShipListItemProps) => (
-  <div className="shipListItem my-10 flex bg-dirt pb-4">
-    <div className="w-9/12 pt-1">
-      <div className="flex">
-        {anchoredShip ? (
-          <Anchor size="2.5rem" className="mt-5 ml-2" />
-        ) : (
-          <div className="mt-5 ml-12" />
-        )}
-        <div className="py-4 px-2">
-          <h2
-            className={`text-2xl font-bold uppercase ${
-              anchoredShip ? "text-lollipop" : "text-vista-blue"
-            }`}
-          >
-            {ship.shipName}
-          </h2>
-          <h3 className={`font-bold ${anchoredShip ? "text-lollipop" : "text-vista-blue"}`}>
-            {ship.date}
-          </h3>
+  <div className="relative">
+    <div className="shipListItem my-10 flex bg-dirt pb-4">
+      <div className="w-full pt-1 md:w-9/12">
+        <div className="flex">
+          {anchoredShip ? (
+            <Anchor size="2.5rem" className="mt-5 ml-2" />
+          ) : (
+            <div className="mt-5 ml-12" />
+          )}
+          <div className="py-4 px-2">
+            <h2
+              className={`text-2xl font-bold uppercase ${
+                anchoredShip ? "text-lollipop" : "text-vista-blue"
+              }`}
+            >
+              {ship.shipName}
+            </h2>
+            <h3 className={`font-bold ${anchoredShip ? "text-lollipop" : "text-vista-blue"}`}>
+              {ship.date}
+            </h3>
+          </div>
+        </div>
+        <div className="ml-10 grid grid-cols-2">
+          <Field label="Fra" value={ship.fromPort} />
+          <Field label="Til" value={ship.toPort} />
+          <Field label="Nasjonalitet" value={ship.nationality} />
+          <Field label="Maks Passasjerer" value={ship.maxPassengers?.toString() ?? "N/A"} />
+          <Field label="Anker Tid" value={ship.anchorTime} />
+          <Field label="Notater" value={ship.notes} />
         </div>
       </div>
-      <div className="ml-10 grid grid-cols-2">
-        <Field label="Fra" value={ship.fromPort} />
-        <Field label="Til" value={ship.toPort} />
-        <Field label="Nasjonalitet" value={ship.nationality} />
-        <Field label="Maks Passasjerer" value={ship.maxPassengers?.toString() ?? "N/A"} />
-        <Field label="Anker Tid" value={ship.anchorTime} />
-        <Field label="Notater" value={ship.notes} />
-      </div>
-    </div>
-    <div className="mt-20">
-      <div className="flex rotate-[270deg] flex-col">
-        <div className="flex justify-between">
-          <Field label="Dag" value={ship.day} styles="capitalize" />
-          <Field label="Dato" value={ship.date} />
+      <div className="mt-[5.5rem] hidden w-60 md:block">
+        <div className="flex rotate-[270deg] flex-col">
+          <div className="mb-1 flex justify-between">
+            <Field label="Dag" value={ship.day} styles="capitalize px-0" />
+            <Field label="Dato" value={ship.date} styles="px-0" />
+          </div>
+          <img src={Barcode} alt="decorative barcode" />
         </div>
-        <img src={Barcode} alt="decorative barcode" />
       </div>
     </div>
   </div>
