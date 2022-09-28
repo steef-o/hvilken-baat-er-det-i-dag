97914164f2eb12cd93e89d2227e04fa7c1f5c344
Steffen Hagen <steffen.hagen@webstep.no>
2022-09-28 13:27:59 +0200

    Add min height to dailyMessage

--- src/components/DailyMessage.tsx
+++ src/components/DailyMessage.tsx
@@ -5,7 +5,7 @@ import { dailyMessage } from "~/state/Atoms";
 const DailyMessage = () => {
   const [message] = useAtom(dailyMessage);
 
-  return <div className="mt-8 text-center text-lg text-dirt">{message}</div>;
+  return <div className="mt-8 min-h-[3.5rem] text-center text-lg text-dirt">{message}</div>;
 };
 
 export default DailyMessage;
