b01834bcd9a83283825ea0c0f96c95f31745393b
Steffen Hagen <steffen.hagen@webstep.no>
2022-10-11 13:49:32 +0200

    Add Jotai debug to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react";
+import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
+import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }), tsconfigPaths()],
   server: {
     port: 3000,
   },
