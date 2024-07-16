c3a84a8d260131a98e2954ef51c782d5df1c2baa
Steffen Andre Hagen <steffen.hagen@bidbax.no>
2024-07-16 23:15:47 +0200

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{ts,tsx}"
@@ -26,4 +26,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
